76ed6ff15550cecce8e54622e436a24a62aae2a0
jefers17 <lenin.puetate@intsuperior.edu.ec>
2023-03-01 17:26:07 -0500

    Primera version del Examen_ssolos_poo_A creacion de clase conexion

---
+++ src/ec/edu/intsuperior/controlador/Conexion.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.controlador;
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author User
+ */
+public class Conexion {
+    static Connection conex=null;
+    
+    public static Connection conexion(){
+        String user="root";
+        String passwd="f2m32831ao";
+        String url="jdbc:mysql://localhost:3306/dbmedidoresaguacariacu?serverTimezone=UTC";
+        try {
+            conex=DriverManager.getConnection(url,user,passwd);
+            System.out.println("Conexion Exitosa");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return conex;
+    }
+}
